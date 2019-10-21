82b7c74e3652f3126fc89914af94f74444c025d7
Leonardo <lnicorena@gmail.com>
2019-10-21 11:36:01 -0300

    refactor(style): added vuetify and some lint config

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,32 +1,39 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view />
-  </div>
+    <v-app>
+        <v-app-bar app>
+            <v-toolbar-title class="headline text-uppercase">
+                <router-link to="/" class="toolbar-title">
+                    <span>CPF &amp; CNPJ</span>
+                    <span class="font-weight-light"> validator</span>
+                </router-link>
+            </v-toolbar-title>
+            <v-spacer></v-spacer>
+            <v-btn text to="/about">
+                <span class="mr-2"
+                    >about<v-icon right>mdi-information-outline</v-icon></span
+                >
+            </v-btn>
+        </v-app-bar>
+
+        <v-content>
+            <router-view />
+        </v-content>
+    </v-app>
 </template>
 
-<style>
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+<style scoped>
+.toolbar-title {
+    color: black;
+    text-decoration: none;
 }
 </style>
+
+<script>
+export default {
+    name: 'App',
+    components: {},
+    data: () => ({
+        //
+    })
+};
+</script>
